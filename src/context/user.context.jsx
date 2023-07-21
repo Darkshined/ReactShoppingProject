2b1b4af3c95a88f84f90ffcf248468772f70fb91
Darkshined <ablazelemon2@gmail.com>
2023-07-21 14:47:48 -0400

    sign in format change

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useReducer } from 'react';
+/*import { createContext, useEffect, useReducer } from 'react';
 
 import {
   onAuthStateChangeListener,
@@ -51,4 +51,4 @@ export const UserProvider = ({ children }) => {
   };
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
-};
+};*/
